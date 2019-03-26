d982811ed000a9c105922c379fb2a84c926e9ae9
GreenLight <1251191746@qq.com>
2019-03-26 16:27:16 +0800

    规范约束布局的使用

---
+++ app/src/main/java/com/gb/vibrator/TestActivity.java
@@ -0,0 +1,12 @@
+package com.gb.vibrator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate( savedInstanceState );
+    setContentView( R.layout.activity_test );
+  }
+}
